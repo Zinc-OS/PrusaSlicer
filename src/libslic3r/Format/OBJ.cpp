08b228f6e9e1a1e2945e8c082c0eed87147fd873
Zinc-OS <louis.sarwal@gmail.com>
2021-07-19 16:55:55 -0400

    add mtl support

--- src/libslic3r/Format/OBJ.cpp
+++ src/libslic3r/Format/OBJ.cpp
@@ -25,10 +25,10 @@ bool mtl_readPNG(char* path, png_structp png_ptr, png_infop info_ptr){
     
     FILE *f= fopen(path,"rb");
     if (!f)
-        return error();
+        return false;
     if (fread(header, 1, number, f) != number)
     {
-       return error();
+       return false;
     }
 
     //int is_png = !png_sig_cmp(*header, 0, number);
@@ -42,12 +42,12 @@ bool mtl_readPNG(char* path, png_structp png_ptr, png_infop info_ptr){
         (PNG_LIBPNG_VER_STRING, (png_voidp)buf,
         (png_error_ptr)NULL, (png_error_ptr)NULL);
     if(!png_ptr){
-        return error();
+        return false;
     }
     info_ptr = png_create_info_struct(png_ptr);
     if (!info_ptr){
        png_destroy_read_struct(&png_ptr,(png_infopp)NULL, (png_infopp)NULL);
-       return error();
+       return false;
     }
     /*if (setjmp(png_jmpbuf(png_ptr)))
     {
@@ -77,17 +77,17 @@ struct color{
     float b;
     void RGBtoCSV(){
         //a triangle
-        if(r-b-g>0){
+        if(r>b&&r>g){
             s=r-(g*b);
             c=r-b/3.0+g/3.0;
             v=r;
         }
-        else if(g>b&&){
+        else if(g>b&&g>r){
             s=g-(g*b);
             c=1.0/3.0+g-r/3.0+b/3.0;
             v=g;
         }
-        else if(b-g-r>0){
+        else if(b>r&&b>g){
             s=b-(g*r);
             c=(2.0/3.0)*b+r/3.0-g/3.0;
             v=b;
@@ -150,8 +150,15 @@ struct color{
             v=0;
     }
 };
-
-bool load_obj(const char *path, TriangleMesh *meshptr)
+int p(int m,int p){
+    int out=1;
+    while(p>0){
+        out*=m;
+        p--;
+    }
+    return out;
+}
+bool load_obj(const char *path, TriangleMesh *meshptr, Model *model,const char *object_name_in)
 {
     if(meshptr == nullptr) return false;
     
@@ -258,191 +265,225 @@ bool load_obj(const char *path, TriangleMesh *meshptr)
         uvData.uvs.emplace_back(uv[1]);
         uvData.uvs.emplace_back(uv[2]);
     }
-   
+    std::string  object_name;
+    if (object_name_in == nullptr) {
+        const char *last_slash = strrchr(path, DIR_SEPARATOR);
+        object_name.assign((last_slash == nullptr) ? path : last_slash + 1);
+    } else
+       object_name.assign(object_name_in);
+    
+    model->add_object(object_name.c_str(), path, std::move(mesh));
     //load mtl data
     ObjParser::parseMTL(path, mtl, data);
-    
-    FacetsAnnotation mmu_seg;
-    TriangleSelector trisel;
-    trisel.CursorType=CIRCLE;
-    trisel.select_patch()
+    int x1;
     //load images, map uvs, and add triangles of different colors
     for(int i=0;i<stl.facet_start.size();i++){
         png_structp png_ptr;
         png_infop info_ptr;
+        //select image;
         for(int i2=0;i2<64;++i2){
-            if(i>idx[i2]){
-                mtl_readPNG(png[mtl_idx[i2]],png_ptr,info_ptr);
-                png_bytepp rows = png_get_rows(png_ptr,info_ptr);
-                png_uint_32 width = png_get_image_width(png_ptr,info_ptr);
-                png_uint_32 height = png_get_image_height(png_ptr,info_ptr);
-                png_byte channels = png_get_channel(png_ptr,info_ptr);
-                png_set_palette_to_rgb(png_ptr,info_ptr);
-                
-                //map uvs
-                int uv1[2] = {uvData.uvs[i*3][0]*width,uvData.uvs[i*3][1]*height};
-                int uv2[2] = {uvData.uvs[i*3+1][0]*width,uvData.uvs[i*3+1][1]*height};
-                int uv3[2] = {uvData.uvs[i*3+2][0]*width,uvData.uvs[i*3+2][1]*height};
-                float slp1=(uv1[1]-uv2[1])/(uv1[0]-uv2[0]);
-                float slp2=(uv2[1]-uv3[1])/(uv2[0]-uv3[0]);
-                float slp3=(uv1[1]-uv3[1])/(uv1[0]-uv3[0]);
-                int iclr1=sqrt(   (uv1[1]-uv2[1])*(uv1[1]-uv2[1]) +   (uv1[0]-uv2[0])*(uv1[0]-uv2[0]) );
-                int iclr2=sqrt(   (uv3[1]-uv2[1])*(uv3[1]-uv2[1]) +   (uv3[0]-uv2[0])*(uv3[0]-uv2[0]) );
-                int iclr3=sqrt(   (uv1[1]-uv3[1])*(uv1[1]-uv3[1]) +   (uv1[0]-uv3[0])*(uv1[0]-uv3[0]) );
-                //make all sides of the triangle the same length(equilateral, for easy conversion to what is presumably subdivided(simple) triangles)
-                int trsil;
-                if(iclr1>iclr2&&iclr1>iclr3){
-                    trsil=iclr1;
-                }
-                else if(iclr2>iclr3&&iclr2>iclr1){
-                    trsil=iclr2;
-                }
-                else if(iclr3>iclr2&&iclr3>iclr1){
-                    trsil=iclr3;
-                }
-                color clr1[trsil];
-                color clr2[trsil];
-                color clr3[trsil];
-                //set color lines
-                {
-                    int nX=uv1[0];
-                    int nY=uv1[1];
-                    int v=0;
-                    while(v<iclr1){
-                        for(int v1=0;v1<abs(uv1[1]-uv2[1]);++v1)//y
-                        {
-                            if(slp1>0){
-                                nY++;
-                            }else{
-                                nY--;
-                            }
-                            clr1[v].R((float)row[(nY*width+nX)*3]/256);
-                            clr1[v].G((float)row[(nY*width+nX)*3+1]/256);
-                            clr1[v].B((float)row[(nY*width+nX)*3+2]/256);
-                            clr1[v].RGBtoCSV();
-                            v++
-
-                        }
-                        for(int v1=0;v1<abs(uv1[0]-uv2[0]);++v1)//x
-                        {
-                            nX++;
-                            clr1[v].R((float)row[(nY*width+nX)*3]/256);
-                            clr1[v].G((float)row[(nY*width+nX)*3+1]/256);
-                            clr1[v].B((float)row[(nY*width+nX)*3+2]/256);
-                            clr1[v].RGBtoCSV();
-                            v++;
-                        }
-                    }
-                }
-
-                {
-                    int nX=uv1[0];
-                    int nY=uv1[1];
-                    int v=0;
-                    while(v<iclr1){
-                        for(int v1=0;v1<abs(uv3[1]-uv2[1]);++v1)//y
-                        {
-                            if(slp2>0){
-                                nY++;
-                            }else{
-                                nY--;
-                            }
-                            clr2[v].R((float)row[(nY*width+nX)*3]/256);
-                            clr2[v].G((float)row[(nY*width+nX)*3+1]/256);
-                            clr2[v].B((float)row[(nY*width+nX)*3+2]/256);
-                            clr2[v].RGBtoCSV();
-                            v++
-
-                        }
-                        for(int v1=0;v1<abs(uv3[0]-uv2[0]);++v1)//x
-                        {
-                            nX++;
-                            clr2[v].R((float)row[(nY*width+nX)*3]/256);
-                            clr2[v].G((float)row[(nY*width+nX)*3+1]/256);
-                            clr2[v].B((float)row[(nY*width+nX)*3+2]/256);
-                            clr2[v].RGBtoCSV();
-                            v++;
-                        }
-                    }
-                }
-
-                {
-                    int nX=uv1[0];
-                    int nY=uv1[1];
-                    int v=0;
-                    while(v<iclr3){
-                        for(int v1=0;v1<abs(uv1[1]-uv3[1]);++v1)//y
-                        {
-                            if(slp3>0){
-                                nY++;
-                            }else{
-                                nY--;
-                            }
-                            clr3[v].R((float)row[(nY*width+nX)*3]/256);
-                            clr3[v].G((float)row[(nY*width+nX)*3+1]/256);
-                            clr3[v].B((float)row[(nY*width+nX)*3+2]/256);
-                            clr3[v].RGBtoCSV();
-                            v++
-
-                        }
-                        for(int v1=0;v1<abs(uv1[0]-uv3[0]);++v1)//x
-                        {
-                            nX++;
-                            clr3[v].R((float)row[(nY*width+nX)*3]/256);
-                            clr3[v].G((float)row[(nY*width+nX)*3+1]/256);
-                            clr3[v].B((float)row[(nY*width+nX)*3+2]/256);
-                            clr3[v].RGBtoCSV();
-                            v++;
-                        }
-                    }
-                }
-                //expand color lines
-                {
-                    color Tclr1[trsil];
-                    color Tclr2[trsil];
-                    color Tclr3[trsil];
-                    for(int b1=0;b1<iclr1;++b1){
-                        Tclr1[(int)(b1*(trsil/iclr1))]=clr1[b1];
-                    }
-                    for(int b1=0;b1<trsil;++b1){
-                        clr1[b1]=Tclr1[b1];
-                    }
-                    for(int b1=0;b1<iclr2;++b1){
-                        Tclr2[(int)(b1*(trsil/iclr2))]=clr2[b1];
-                    }
-                    for(int b1=0;b1<trsil;++b1){
-                        clr2[b1]=Tclr2[b1];
-                    }
-                    for(int b1=0;b1<iclr3;++b1){
-                        Tclr3[(int)(b1*(trsil/iclr3))]=clr3[b1];
-                    }
-                    for(int b1=0;b1<trsil;++b1){
-                        clr3[b1]=Tclr3[b1];
-                    }
-                    
-                }
-                //now fill the inside(s)
-
-                
-
-
-                //..
-                
-                //    ^
-                //   /_\
-
-                   
-                
-
+            if(i>mtl.idx[i2]){
+                x1=i2;
             }
         }
+        int i2=x1;
+        mtl_readPNG(mtl.png[mtl.mtl_idx[i2]],png_ptr,info_ptr);
+        png_bytepp rows = png_get_rows(png_ptr,info_ptr);
+        png_uint_32 width = png_get_image_width(png_ptr,info_ptr);
+        png_uint_32 height = png_get_image_height(png_ptr,info_ptr);
+        png_byte channels = png_get_channels(png_ptr,info_ptr);
+        png_set_palette_to_rgb(png_ptr);
+        
+        //map uvs
+        int uv1[2] = {uvData.uvs[i*3][0]*width,uvData.uvs[i*3][1]*height};
+        int uv2[2] = {uvData.uvs[i*3+1][0]*width,uvData.uvs[i*3+1][1]*height};
+        int uv3[2] = {uvData.uvs[i*3+2][0]*width,uvData.uvs[i*3+2][1]*height};
+        float slp1=(uv1[1]-uv2[1])/(uv1[0]-uv2[0]);
+        float slp2=(uv2[1]-uv3[1])/(uv2[0]-uv3[0]);
+        float slp3=(uv1[1]-uv3[1])/(uv1[0]-uv3[0]);
+        int iclr1=sqrt(   (uv1[1]-uv2[1])*(uv1[1]-uv2[1]) +   (uv1[0]-uv2[0])*(uv1[0]-uv2[0]) );
+        int iclr2=sqrt(   (uv3[1]-uv2[1])*(uv3[1]-uv2[1]) +   (uv3[0]-uv2[0])*(uv3[0]-uv2[0]) );
+        int iclr3=sqrt(   (uv1[1]-uv3[1])*(uv1[1]-uv3[1]) +   (uv1[0]-uv3[0])*(uv1[0]-uv3[0]) );
+        //make all sides of the triangle the same length(equilateral, for easy conversion to what is presumably subdivided(simple) triangles)
+        int trsil;
+        if(iclr1>iclr2&&iclr1>iclr3){
+            trsil=iclr1;
+        }
+        else if(iclr2>iclr3&&iclr2>iclr1){
+            trsil=iclr2;
+        }
+        else{
+            trsil=iclr3;
+        }
+        //rebase trsil(longest side of triangle) to closest power of 2
+        int mag=0;
+        while(trsil>0){
+            mag+=1;
+            if(trsil==p(mag,2)){
+                trsil=-1;
+            }
+            trsil-=trsil%(p(mag,2));
+            
+        }
+        mag--;
+        trsil=p(mag,2);
+    
+        color clr1[trsil];
+        color clr2[trsil];
+        color clr3[trsil];
+        //set color lines
+        {
+            int nX=uv1[0];
+            int nY=uv1[1];
+            int v=0;
+            while(v<iclr1){
+                for(int v1=0;v1<abs(uv1[1]-uv2[1]);++v1)//y
+                {
+                    if(slp1>0){
+                        nY++;
+                    }else{
+                        nY--;
+                    }
+                    clr1[v].R(float(rows[nY*3][nX]/256.0));
+                    clr1[v].G(float(rows[nY*3+1][nX]/256.0));
+                    clr1[v].B(float(rows[nY*3+2][nX]/256.0));
+                    clr1[v].RGBtoCSV();
+                    v++;
+
+                }
+                for(int v1=0;v1<abs(uv1[0]-uv2[0]);++v1)//x
+                {
+                    nX++;
+                    clr1[v].R(float(rows[nY*3][nX]/256.0));
+                    clr1[v].G(float(rows[nY*3+1][nX]/256.0));
+                    clr1[v].B(float(rows[nY*3+2][nX]/256.0));
+                    clr1[v].RGBtoCSV();
+                    v++;
+                }
+            }
+        }
+
+        {
+            int nX=uv1[0];
+            int nY=uv1[1];
+            int v=0;
+            while(v<iclr1){
+                for(int v1=0;v1<abs(uv3[1]-uv2[1]);++v1)//y
+                {
+                    if(slp2>0){
+                        nY++;
+                    }else{
+                        nY--;
+                    }
+                    clr2[v].R(float(rows[nY*3][nX]/256.0));
+                    clr2[v].G(float(rows[nY*3+1][nX]/256.0));
+                    clr2[v].B(float(rows[nY*3+2][nX]/256.0));
+                    clr2[v].RGBtoCSV();
+                    v++;
+
+                }
+                for(int v1=0;v1<abs(uv3[0]-uv2[0]);++v1)//x
+                {
+                    nX++;
+                    clr2[v].R(float(rows[nY*3][nX]/256.0));
+                    clr2[v].G(float(rows[nY*3+1][nX]/256.0));
+                    clr2[v].B(float(rows[nY*3+2][nX]/256.0));
+                    clr2[v].RGBtoCSV();
+                    v++;
+                }
+            }
+        }
+
+        {
+            int nX=uv1[0];
+            int nY=uv1[1];
+            int v=0;
+            while(v<iclr3){
+                for(int v1=0;v1<abs(uv1[1]-uv3[1]);++v1)//y
+                {
+                    if(slp3>0){
+                        nY++;
+                    }else{
+                        nY--;
+                    }
+                    clr3[v].R(float(rows[nY*3][nX]/256.0));
+                    clr3[v].G(float(rows[nY*3+1][nX]/256.0));
+                    clr3[v].B(float(rows[nY*3+2][nX]/256.0));
+                    clr3[v].RGBtoCSV();
+                    v++;
+
+                }
+                for(int v1=0;v1<abs(uv1[0]-uv3[0]);++v1)//x
+                {
+                    nX++;
+                    clr3[v].R(float(rows[nY*3][nX]/256.0));
+                    clr3[v].G(float(rows[nY*3+1][nX]/256.0));
+                    clr3[v].B(float(rows[nY*3+2][nX]/256.0));
+                    clr3[v].RGBtoCSV();
+                    v++;
+                }
+            }
+        }
+        //expand color lines
+        {
+            color Tclr1[trsil];
+            color Tclr2[trsil];
+            color Tclr3[trsil];
+            for(int b1=0;b1<iclr1;++b1){
+                Tclr1[(int)(b1*(trsil/iclr1))]=clr1[b1];
+            }
+            for(int b1=0;b1<trsil;++b1){
+                clr1[b1]=Tclr1[b1];
+            }
+            for(int b1=0;b1<iclr2;++b1){
+                Tclr2[(int)(b1*(trsil/iclr2))]=clr2[b1];
+            }
+            for(int b1=0;b1<trsil;++b1){
+                clr2[b1]=Tclr2[b1];
+            }
+            for(int b1=0;b1<iclr3;++b1){
+                Tclr3[(int)(b1*(trsil/iclr3))]=clr3[b1];
+            }
+            for(int b1=0;b1<trsil;++b1){
+                clr3[b1]=Tclr3[b1];
+            }
+            
+        }
+        //..
+        //split triangle data
+        //0011=split 
+        //fractal
+        //1-4-16-64
+
+        
+        std::pair<std::vector<std::pair<int, int>>, std::vector<bool>> data;
+        assert(data.first.empty() || data.first.back().first < i);
+        data.first.emplace_back(i,int(data.second.size()));
+        for(int id=0; id<mag; id++){
+            data.second.push_back(0);
+            data.second.push_back(0);
+            data.second.push_back(1);
+            data.second.push_back(1);
+        }
+        //test
+        for(int id=0; id<p(mag,4);id++){
+            data.second.push_back(0);
+            data.second.push_back(0);
+            data.second.push_back(0);
+            data.second.push_back(1);
+        }
+        std::string STR="";
+        for(int n=0;n<data.second.size();n+=4){
+            char ch=data.second[n]*0x1+data.second[n+1]*0x2+data.second[n+2]*0x4+data.second[n+3]*0x8;
+            STR[n]=ch;
+        }
+        //what a terrible workaround. 
+        model->objects[0]->volumes[0]->mmu_segmentation_facets.set_triangle_from_string(i,STR);
     }
 
 
-    //..
-    assert(mmu_seg.m_data.first.empty() || mmu_seg.m_data.first.back().first < i);
-    mmu_seg.m_data.first.emplace_back(i,int(mmu_seg.m_data.second.size()));)
-    mmu_seg.m_data.second.insert(m_data.second.end(),true);   
+      
+    
      
     stl_get_size(&stl);
     mesh.repair();
@@ -457,18 +498,11 @@ bool load_obj(const char *path, TriangleMesh *meshptr)
 bool load_obj(const char *path, Model *model, const char *object_name_in)
 {
     TriangleMesh mesh;
-    
-    bool ret = load_obj(path, &mesh);
+    bool ret = load_obj(path, &mesh, model,object_name_in);
     
     if (ret) {
-        std::string  object_name;
-        if (object_name_in == nullptr) {
-            const char *last_slash = strrchr(path, DIR_SEPARATOR);
-            object_name.assign((last_slash == nullptr) ? path : last_slash + 1);
-        } else
-           object_name.assign(object_name_in);
-    
-        model->add_object(object_name.c_str(), path, std::move(mesh));
+        
+        
     }
     
     return ret;
